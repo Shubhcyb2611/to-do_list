e51a2da1341199b1399be4598c9c046b8a496db1
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-07-10 14:46:59 +0530

    feat: database coonnection added

--- src/infrastructure/database.config.ts
+++ src/infrastructure/database.config.ts
@@ -1,24 +1,3 @@
-// import { DataSource } from "typeorm";
-// import dotenv from "dotenv";
-// import { fileURLToPath } from "url";
-// import path from "path";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// dotenv.config({ path: path.resolve(__dirname, "../../.env") });
-
-// const { DATABASE_URL } = process.env;
-
-// const entities = [path.resolve(__dirname, "../domain/entities/*.{ts,js}")];
-// export const AppDataSource = new DataSource({
-//   type: "postgres",
-//   url: DATABASE_URL,
-//   entities,
-//   synchronize: false,
-//   migrations: [path.resolve(__dirname, "./migrations/*.{ts,js}")],
-// });
-
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
