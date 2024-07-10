0a2ccfa1da2106217bc316eb140f1ec8c421c051
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-07-10 17:57:13 +0530

    feat : jwt added

---
+++ src/infrastructure/migrations/1720613983652-migration.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1720613983652 implements MigrationInterface {
+    name = 'Migration1720613983652'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ADD "email" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "password" character varying`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "password"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "email"`);
+    }
+
+}
